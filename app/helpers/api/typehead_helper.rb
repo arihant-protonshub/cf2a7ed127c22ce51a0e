0a3cbf4ecd4301e1f2f091c6338ff09574192237
arihant nahar <arihant.nahar@protonshub.com>
2021-04-13 20:30:34 +0530

    initial commit

---
+++ app/helpers/api/typehead_helper.rb
@@ -0,0 +1,2 @@
+module Api::TypeheadHelper
+end
---
+++ test/controllers/api/typehead_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::TypeheadControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
